c5af7f359685b7e7350d9a05361e75e9eed747a7
Bhavik Joshi <bhavikjoshi29@gmail.com>
2017-01-15 18:56:52 +0530

    Correction 1

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const publicPath = path.join(__dirname , '../public');
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
-    console.log(`Server is up on port 3000`);
-})
+    console.log(`Server is up on port ${port}`);
+});
